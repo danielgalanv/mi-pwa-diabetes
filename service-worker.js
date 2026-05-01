428a8801dd5745025a83b374e612f0dd431a31f7
danielgalanv <daniel.galan@upm.es>
2026-05-01 18:55:53 +0200

    arreglao

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,32 @@
-const CACHE_NAME = "mi-pwa-diabetes-v2";
+const CACHE_NAME = "mi-pwa-diabetes-v3";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./style.css",
+  "./app.js",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-  console.log("SW: instalando");
-
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log("SW: cache abierta");
-        return cache.addAll(FILES_TO_CACHE);
-      })
-      .then(() => {
-        console.log("SW: archivos cacheados correctamente");
-        return self.skipWaiting();
-      })
-      .catch(error => {
-        console.error("SW: error cacheando archivos", error);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  console.log("SW: activado");
-
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+
+
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
